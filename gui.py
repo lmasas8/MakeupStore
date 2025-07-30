25874201a0a2766bb40af926b7ee89b707257494
Almas Asiri <almassyry1424@gmail.com>
2025-07-30 21:02:26 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
 # gui.py
-# by: Almas Yahya Ali 443805688 , Noura mohaya 445803907 , Hanin Omar 444806869 , Ghaida Saeed 444810066
 
 import tkinter as tk
 from tkinter import messagebox, Toplevel, Label
